1a875d4aa7dfc377cf4fd93b59fc58cf4bb5bf15
Chaitanya Patil <chaitanya.patil@sakec.ac.in>
2020-05-20 21:17:31 +0530

    mongoose depraciated error

--- app.js
+++ app.js
@@ -4,14 +4,15 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 require('dotenv').config();
-#change hua ka checking
-#change 2 forking
+//change hua ka checking
+//change 2 forking
 mongoose.connect(
     'mongodb+srv://chaitanyapatil927:' + process.env.MONGO_ATLAS_PW + '@node-rest-codeblog-9hddi.mongodb.net/test?retryWrites=true&w=majority',
     {
         useMongoClient: true
     }
 );
+mongoose.Promise = global.Promise;
 
 
 const codeRoutes = require('./api/routes/codes');
